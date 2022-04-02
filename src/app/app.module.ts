e60838f1996750df0c8c46f68ac7fc182b5d46bd
vanloik2 <nguyenvanloi0105212@gmail.com>
2022-04-02 09:37:48 +0700

    layout admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AdminHeaderComponent } from './layout/admin/admin-header/admin-header.component';
+import { AdminMainComponent } from './layout/admin/admin-main/admin-main.component';
+import { AdminAsideComponent } from './layout/admin/admin-aside/admin-aside.component';
+import { AdminFooterComponent } from './layout/admin/admin-footer/admin-footer.component';
+import { ClientHeaderComponent } from './layout/client/client-header/client-header.component';
+import { ClientFooterComponent } from './layout/client/client-footer/client-footer.component';
+import { ClientMainComponent } from './layout/client/client-main/client-main.component';
+import { LayoutAdminComponent } from './layout/layout-admin/layout-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminHeaderComponent,
+    AdminMainComponent,
+    AdminAsideComponent,
+    AdminFooterComponent,
+    ClientHeaderComponent,
+    ClientFooterComponent,
+    ClientMainComponent,
+    LayoutAdminComponent
   ],
   imports: [
     BrowserModule,
